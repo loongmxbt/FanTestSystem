84d75b60087a3adf4ec15df8604b69fe3fcb5ee0
loongmxbt <loongmxbt@gmail.com>
2018-03-08 22:26:00 +0800

    chart

--- renderer-process/fan/draw-chart.js
+++ renderer-process/fan/draw-chart.js
@@ -2,14 +2,46 @@ const Chart = require('chart.js');
 
 const ctx = document.getElementById("pqChart");
 
+const pq_data = [{
+                     x: 0,
+                     y: 120
+                 }, {
+                     x: 1000,
+                     y: 112
+                 }, {
+                     x: 2000,
+                     y: 103
+                 }, {
+                     x: 3000,
+                     y: 76
+                 }, {
+                     x: 4000,
+                     y: 65
+                 }, {
+                     x: 5000,
+                     y: 54
+                 }, {
+                     x: 6000,
+                     y: 42
+                 }]
+
+
 var pqChart = new Chart(ctx, {
-  type: 'line',
-  data:{ "labels": [ "January", "February", "March", "April", "May", "June", "July" ],
-         "datasets": [{ "label": "My First Dataset",
-                        "data": [65,59,80,81,56,55,40],
-                        "fill": false,
-                        "borderColor": "rgb(75, 192, 192)",
-                        "lineTension": 0.1}]
-        },
-  options: {}
+    type: 'line',
+    data: {
+        datasets: [{
+            label: 'P-Q 曲线',
+            data: pq_data,
+            fill: false,
+            borderColor: "rgb(75, 192, 192)"
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
+            }]
+        }
+    }
 });
